ba333106f20a98bb21b2032af2d05a6b9094624c
AngelMD55 <angel_demerutis@hotmail.com>
2019-02-14 18:32:40 -0700

    fixed line 26 changed type boolean to type datatypesboolean

--- models/trucksModel.js
+++ models/trucksModel.js
@@ -23,7 +23,7 @@ module.exports = function(sequelize, DataTypes) {
             type: DataTypes.STRING           
         },
         truckLive:{
-            type: BOOLEAN,
+            type: DataTypes.BOOLEAN,
             defaultValue: '0'
         }
     }, {
