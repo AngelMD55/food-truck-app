d643f82afdf5ead5f894ddf4ba1f9b1e6f442c9f
Your Name <you@example.com>
2019-02-14 21:02:31 -0700

    adding api-keys

--- models/schema.sql
+++ models/schema.sql
@@ -9,46 +9,30 @@ CREATE TABLE users(
     lastName VARCHAR(50) NOT NULL,
     userName VARCHAR(50) NOT NULL,
     userPassword BLOB NOT NULL,
+    userType VARCHAR(15) NOT NULL,
     userPhoneNum CHAR NOT NULL,
     userEmail VARCHAR(100) NOT NULL,
-    userBday DATE, 
+    userBday DATE,
+    dateJoined DATE NOT NULL,
     paidUser BOOLEAN NOT NULL
-
-);
-
-CREATE TABLE adminUsers(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    adminFirstName VARCHAR(50) NOT NULL,
-    adminLastName VARCHAR(50) NOT NULL,
-    adminUserName VARCHAR(50) NOT NULL,
-    adminUserPassword BLOB NOT NULL,
-    adminEmail VARCHAR(100) NOT NULL,
-    paidAdminUser BOOLEAN NOT NULL,
-    adminJoinDate DATE NOT NULL,
 
 );
 
 CREATE TABLE trucks(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     truckName VARCHAR(50) NOT NULL,
-    adminUserName VARCHAR(50) NOT NULL,
+    UserName VARCHAR(50) NOT NULL,
     priceRange CHAR(4)
-
-);
-
-CREATE TABLE foodCategories (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    truckName VARCHAR(50) NOT NULL,
-    userName VARCHAR(50) NOT NULL,
     foodCategory1 VARCHAR(20) NOT NULL,
     foodCategory2 VARCHAR(20),
     foodCategory3 VARCHAR(20)
 
-)
-
-CREATE TABLE userFavorites(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    userName VARCHAR(50) NOT NULL,
-    truckName VARCHAR(50) NOT NULL,
-
 );
+
+
+-- CREATE TABLE userFavorites(
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     userName VARCHAR(50) NOT NULL,
+--     truckName VARCHAR(50) NOT NULL,
+
+-- );
