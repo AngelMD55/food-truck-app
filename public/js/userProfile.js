93d76a3f5521e9652578e05d89b80143ea9caa03
AngelMD55 <angel_demerutis@hotmail.com>
2019-02-18 18:39:16 -0700

    getting ready to pull master

--- public/js/userProfile.js
+++ public/js/userProfile.js
@@ -1,41 +1,54 @@
 $(document).ready(function () {
   let user;
+  let firstName;
+  let lastName;
+  let userName;
+  let userPassword;
+  let userType;
+  let userEmail;
 
   $("#signUpSubmit").on("click", function (event) {
     event.preventDefault();
+
+    //Passing values to variables
+    firstName = $("#userFirstName").val().trim(),
+    lastName = $("#userLastName").val().trim(),
+    userName = $("#userName").val().trim(),
+    userPassword = $("#password").val().trim(),
+    userType = $("input[name='type']:checked").val(),
+    userEmail = $("#userEmail").val().trim()
+
     //Creating object to pass to database
     let newUserSignUp = {
-      firstName: $("#userFirstName").val().trim(),
-      lastName: $("#userLastName").val().trim(),
-      userPassword: $("#password").val().trim(),
-      userType: $("input[name='type']:checked").val(),
-      userEmail: $("#userEmail").val().trim()
+      firstName: firstName,
+      lastName: lastName,
+      userName: userName,
+      userPassword: userPassword,
+      userType: userType,
+      userEmail: userEmail
     }
     console.log(newUserSignUp);
     $.ajax({
       method: "POST",
       url: "/api/users",
       data: newUserSignUp
-    }).done(function (msg) {
-      alert("Data Saved")
-      window.location.href = "/userp";
+    }).then(function () {
+      window.location.replace("/userp");
     });
   });
 
-  // function getID(){
-  //   $.get("/api/users", function(data){
-  //     user = data;
-  //     console.log(user)
-  //   })
-  // }
+  function getID(){
+    $.get("/api/users", function(data){
+      user = data;
+    })
+  }
 
-  // getID();
+  getID();
 
-  $("#userSubmit").on("click", function(event){
+  $("#userSubmit").on("click", function (event) {
     event.preventDefault();
-    
+
     let updateUserInfo = {
-      userName: $("#userName").val().trim(),
       userPhoneNum: $("#userPhoneNum").val().trim(),
       userAddress: $("#userAddress").val().trim(),
       userCity: $("#userCity").val().trim(),
@@ -44,13 +57,13 @@ $(document).ready(function () {
       userGender: $("input[name='gender']:checked").val(),
       userBday: $("#birthDate").val().trim(),
     }
-    console.log(updateUserInfo);
+    // console.log(updateUserInfo);
     $.ajax({
       method: "PUT",
       url: "/api/users/",
       data: updateUserInfo
-    }).then(function(){
-      window.location.href = "/userv";
+    }).then(function (data) {
+      window.location.replace(`/userv/${data.id}`);
     })
   })
 });
