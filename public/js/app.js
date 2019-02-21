7e5f7d3def3b731d5ac540592747231270a0d5f4
Your Name <you@example.com>
2019-02-20 22:01:08 -0700

    oops sorry team I was working on the master

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,7 @@
+// $(document).ready(function(){
 let truck = $("#truckName").text();
+console.log("this is the truck name" + truck);
+
 // array of truck objects
 let truckLocations = [];
 
@@ -11,14 +14,13 @@ function getPosition() {
 			const lat = position.coords.latitude;
 			const lng = position.coords.longitude;
 
-			let truckOb = {
-				truckName: truck,
-				lat: lat,
-				lng: lng,
-			}
+			// let truckOb = {
+			// 	truckName: truck,
+			// 	lat: lat,
+			// 	lng: lng,
+			// }
 
-			console.log(truckOb);
-			truckLocations.push(truckOb)
+			// console.log(truckOb);
 
 			console.log(`longitude: ${lng} | latitude: ${lat}`);
 		}
@@ -26,10 +28,10 @@ function getPosition() {
 		console.log("browser doesn't support geolocation api")
 	}
 
-	$("#vendorCheckIn").on("click", function (position) {
-		console.log(truckLocations);
+	// $("#vendorCheckIn").on("click", function (position) {
+	// 	console.log(truckLocations);
 
-	})
+	// })
 }
 getPosition();
 
@@ -38,27 +40,65 @@ var map, infoWindow;
 
 function initMap() {
 	map = new google.maps.Map(document.getElementById("map"), {
-		center: {lat: 32.2226, lng: 110.9747},
-		zoom: 6
+		center: { lat: 32.2341679, lng: -110.9465769 },
+		zoom: 10
 	});
 	console.log(map);
 
 	infoWindow = new google.maps.InfoWindow;
 
-	// Try HTML5 geolocation.
+
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(function (position) {
+
+			let lat= position.coords.latitude;
+			let lng= position.coords.longitude;
+
 			var pos = {
 				lat: position.coords.latitude,
 				lng: position.coords.longitude
 			};
 
-			console.log(pos);
+			let truckOb = {
+				truckName: truck,
+				lat: lat,
+				lng: lng,
+			};
+
+			marker.setIcon(({
+				url: pos.icon,
+				size: new google.maps.Size(71, 71),
+				origin: new google.maps.Point(0, 0),
+				anchor: new google.maps.Point(17, 34),
+				scaledSize: new google.maps.Size(35, 35)
+			}));
+
+
+			let infoContainer = document.createElement('div');
+			let infoText = document.createElement('p');
+			infoText.innerHTML = "You're Here!";
+			var checkInBtn = document.createElement('button');
+			checkInBtn.innerHTML = "Check In";
+			infoContainer.appendChild(infoText);
+			infoContainer.appendChild(checkInBtn);
 
 			infoWindow.setPosition(pos);
-			infoWindow.setContent('Location found.');
+			infoWindow.setContent(infoContainer);
 			infoWindow.open(map);
 			map.setCenter(pos);
+
+			google.maps.event.addListener(marker, 'click', function () {
+				infoWindow.setContent(infoContainer);
+				infoWindow.open(map, marker);
+			});
+
+			google.maps.event.addListener(infoWindow, 'domready', function () {
+				google.maps.event.addDomListener(checkInBtn, 'click', function () {
+					truckLocations.push(truckOb);
+					console.log(pos)
+				});
+			});
+
 		}, function () {
 			handleLocationError(true, infoWindow, map.getCenter());
 		});
@@ -66,22 +106,7 @@ function initMap() {
 		// Browser doesn't support Geolocation
 		handleLocationError(false, infoWindow, map.getCenter());
 	}
-}
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-	infoWindow.setPosition(pos);
-	infoWindow.setContent(browserHasGeolocation ?
-		'Error: The Geolocation service failed.' :
-		'Error: Your browser doesn\'t support geolocation.');
-	infoWindow.open(map);
-}
-
-// using places api with search autocomplete
-function initMap() {
-	var map = new google.maps.Map(document.getElementById('map'), {
-		center: { lat: 32.2341679, lng: -110.9465769 },
-		zoom: 13
-	});
+	// Update when searching
 	var input = document.getElementById('vendorAddress');
 
 	map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
@@ -89,14 +114,14 @@ function initMap() {
 	var autocomplete = new google.maps.places.Autocomplete(input);
 	autocomplete.bindTo('bounds', map);
 
-	var infowindow = new google.maps.InfoWindow();
 	var marker = new google.maps.Marker({
 		map: map,
 		anchorPoint: new google.maps.Point(0, -29)
 	});
 
 	autocomplete.addListener('place_changed', function () {
-		infowindow.close();
+
+		infoWindow.close();
 		marker.setVisible(false);
 		var place = autocomplete.getPlace();
 
@@ -127,35 +152,53 @@ function initMap() {
 			].join(' ');
 		}
 
-		checkInLocation = [{
+		// let lat= place.geometry.location.lat;
+		// let lng= place.geometry.location.lng;
+
+		let checkInLocation = [{
 			lat: place.geometry.location.lat(),
 			lng: place.geometry.location.lng()
 		}];
 
-		var checkInBtn = '<button id= "checkIn>Check-In</button>';
+		let truckOb = {
+			truckName: truck,
+			lat: place.geometry.location.lat(),
+			lng: place.geometry.location.lng()
+		};
+		console.log(truckOb);
+
+		let infoContainer = document.createElement('div');
+		let infoText = document.createElement('p');
+		infoText.innerHTML = address;
+		var checkInBtn = document.createElement('button');
+		checkInBtn.innerHTML = "Check In";
+		infoContainer.appendChild(infoText);
+		infoContainer.appendChild(checkInBtn);
+
 		google.maps.event.addListener(marker, 'click', function () {
-			infowindow.setContent(address + checkInBtn);
-			infowindow.open(map, marker);
+			infoWindow.setContent(infoContainer);
+			infoWindow.open(map, marker);
 		});
 
-		google.maps.event.addListener(infowindow, 'domready', function () {
+		google.maps.event.addListener(infoWindow, 'domready', function () {
 			google.maps.event.addDomListener(checkInBtn, 'click', function () {
-				console.log(checkInLocation)
+				truckLocations.push(truckOb);
+				console.log("this is the truck locations array" + JSON.stringify(truckLocations));
+
 			});
 		});
-		
-		// infowindow = new google.maps.InfoWindow({
-		//     content: " "
-		// });
-		// infowindow.open(map, marker);
 
-		// infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address + '<br><div> </div>' + '<br><button id= "checkIn>Check-In</button>');
-		// infowindow.open(map, marker)
-
-		// console.log(truckLocation);
-	});
-
+	})
 }
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+	infoWindow.setPosition(pos);
+	infoWindow.setContent(browserHasGeolocation ?
+		'Error: The Geolocation service failed.' :
+		'Error: Your browser doesn\'t support geolocation.');
+	infoWindow.open(map);
+}
+// });
 
 // to loop through trucklocations and place multiple markers
 // 	var marker, i;
